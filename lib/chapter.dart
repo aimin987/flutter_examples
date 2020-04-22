75f009fdd1810d704faea06f87803c8d768f6af3
Aimin <aimin987@163.com>
2020-04-22 15:27:48 +0800

    增加目录

---
+++ lib/chapter.dart
@@ -0,0 +1,20 @@
+/*
+ * File: chapter.dart
+ * -----
+ * Description: 
+ * -----
+ * Created  By: Aim 2020-04-22 15:00:17
+ * Modified By: Aim 2020-04-22 15:00:22
+ * -----
+ * HISTORY:
+ * Date      	By	Comments
+ * ----------	---	----------------------------------------------------------
+ */
+
+
+class Chapter {
+  Chapter(this.title, this.route);
+
+  final String title;
+  final String route;
+}
